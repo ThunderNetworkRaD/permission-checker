c5fdf2895ad0c1210e37c041be13c58799049ac4
KillerBossOriginal <killerbossoriginal@outlook.it>
2024-07-04 21:38:57 +0200

    First Commit

---
+++ build.gradle.kts
@@ -0,0 +1,72 @@
+plugins {
+    kotlin("multiplatform") version "2.0.0"
+    id("maven-publish")
+}
+
+group = "org.thundernetwork.permissionchecker"
+version = "2.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+}
+
+kotlin {
+    jvm {}
+    js {
+        nodejs {
+        }
+        binaries.executable()
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+            }
+        }
+        val jvmMain by getting {
+            dependencies {
+            }
+        }
+        val jsMain by getting {
+            dependencies {
+            }
+        }
+    }
+}
+
+tasks {
+    val jsProductionExecutableCompileSync by getting
+    val jsNodeRun by getting {
+        dependsOn(jsProductionExecutableCompileSync)
+    }
+    val jsNodeDevelopmentRun by getting {
+        dependsOn(jsProductionExecutableCompileSync)
+    }
+    val jsDevelopmentExecutableCompileSync by getting
+    val jsNodeProductionRun by getting {
+        dependsOn(jsDevelopmentExecutableCompileSync)
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["kotlin"])
+            groupId = project.group.toString()
+            artifactId = "permission-checker"
+            version = project.version.toString()
+        }
+    }
+    repositories {
+        maven {
+            url = uri("https://path/to/your/maven/repository") // Replace with your Maven repository URL
+            credentials {
+                username = "your-username" // Replace with your Maven repository username
+                password = "your-password" // Replace with your Maven repository password
+            }
+        }
+    }
+}
---
+++ src/commonMain/kotlin/App.kt
@@ -0,0 +1,33 @@
+package org.thundernetwork.permissionchecker
+
+fun checkSingle(permission: String, requiredPermission: String): Boolean {
+    var answer = false
+    if (permission === "*") answer = true
+    else {
+        val subRP = requiredPermission.split('.')
+        val sub = permission.split('.')
+        if (subRP.size < sub.size) answer = false
+        else {
+            var i = 0
+            for (rp in subRP) {
+                if (i == sub.size || rp != sub[i]) break
+                else i++
+            }
+            if (i == subRP.size || i == sub.size) answer = true
+        }
+    }
+    return answer
+}
+
+fun checkList(permission: Array<String>, requiredPermission: Array<String>): Boolean {
+    var actualLength = 0
+    val requiredLength = requiredPermission.size
+
+    for (rp in requiredPermission) {
+        for (p in permission) {
+            if (checkSingle(p, rp)) actualLength++
+        }
+    }
+
+    return actualLength >= requiredLength
+}
